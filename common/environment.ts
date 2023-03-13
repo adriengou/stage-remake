4b08d3ed415436f5efe62ba87b5f50d7b3554b3c
Adrien Gouacide <adrien.gouacide@gmail.com>
2023-03-13 22:36:52 +0100

    commit

---
+++ common/environment.ts
@@ -0,0 +1,9 @@
+export const HOST = "http://127.0.0.1";
+export const PORT = 8888;
+export const DB_CONNECTION = {
+  host: "127.0.0.1",
+  port: 3456,
+  user: "postgres",
+  password: "testPassword123",
+  database: "stage",
+};
