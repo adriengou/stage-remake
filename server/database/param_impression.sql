b48ded0d1375b296fbc029b02795b30725b3749e
Adrien Gouacide <adrien.gouacide@gmail.com>
2023-03-14 17:07:13 +0100

    commit

---
+++ server/database/param_impression.sql
@@ -0,0 +1,25 @@
+-- Active: 1678783217472@@127.0.0.1@5433@stage
+
+-- create
+
+INSERT INTO
+    param_impression (
+        nom,
+        taille_police,
+        alignement,
+        distance_haut,
+        cadre,
+        libelle,
+        distance_bas
+    )
+VALUES (
+        'test',
+        5,
+        'gauche',
+        10,
+        false,
+        true,
+        15
+    );
+
+UPDATE 
